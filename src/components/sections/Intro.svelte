6178059a6da8d822e8e135e23dbabbff2a425917
Dani-lin-desk <dani.linux.97@gmail.com>
2023-08-02 22:53:54 +0200

    Add intro text

---
+++ src/components/sections/Intro.svelte
@@ -0,0 +1,39 @@
+<script>
+	import typeVFX from '$lib/writing.mp3';
+	import { onMount } from 'svelte';
+</script>
+
+<div>
+	<h1
+		class="text-primary font-bold border-r-2 border-primary mx-auto whitespace-nowrap overflow-hidden text-2xl md:text-6xl p-2"
+	>
+		Coding is hobby not work !
+	</h1>
+</div>
+
+<style lang="scss">
+	div {
+		h1 {
+			animation: typing 3.5s steps(30, end), blink-caret 0.5s step-end infinite;
+		}
+	}
+
+	@keyframes typing {
+		from {
+			width: 0;
+		}
+		to {
+			width: 100%;
+		}
+	}
+
+	@keyframes blink-caret {
+		from,
+		to {
+			border-color: transparent;
+		}
+		50% {
+			border-color: #7ed957;
+		}
+	}
+</style>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,6 @@
+<script>
+	import Intro from "../components/sections/Intro.svelte";
+
+</script>
+
+<Intro/>
