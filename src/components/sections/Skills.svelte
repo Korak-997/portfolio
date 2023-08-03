b82fb1827508e57285bcb16d232807c198472fca
Dani-lin-desk <dani.linux.97@gmail.com>
2023-08-03 12:30:33 +0200

    Add skills icons

---
+++ src/components/sections/Skills.svelte
@@ -0,0 +1,20 @@
+<script>
+	import skills from '../store/skills.js';
+
+	let icons;
+	skills.subscribe((data) => {
+		icons = data;
+	});
+</script>
+
+<div>
+	<h1 class="text-primary font-bold text-4xl md:text-6xl p-2">Skills</h1>
+	<div class="flex items-center justify-around flex-wrap">
+		{#each icons as icon}
+			<img class="w-24 m-2" src={icon.cdn} alt="skill logo" />
+		{/each}
+	</div>
+</div>
+
+<style>
+</style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,6 +1,8 @@
 <script>
 	import Intro from "../components/sections/Intro.svelte";
+	import Skills from "../components/sections/Skills.svelte";
 
 </script>
 
 <Intro/>
+<Skills/>
