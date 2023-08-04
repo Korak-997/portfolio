f8d24125df216fe8dae7c1f379ba7182e221b685
Dani-lin-desk <dani.linux.97@gmail.com>
2023-08-04 13:03:21 +0200

    Add inlang for i18n support

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,9 +3,10 @@
 	import Skills from "../components/sections/Skills.svelte";
 	import Socials from "../components/sections/Socials.svelte";
 	import TimeLine from "../components/sections/TimeLine.svelte";
+  import { i } from '@inlang/sdk-js'
 
 </script>
-
+<h1>{i('welcome')}</h1>
 <Intro/>
 <TimeLine/>
 <Skills/>
