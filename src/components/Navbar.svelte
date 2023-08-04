12960cc1d57971506197d5240612a481ba304d3d
Dani-lin-desk <dani.linux.97@gmail.com>
2023-08-04 13:55:53 +0200

    Add Language selector

--- src/components/Navbar.svelte
+++ src/components/Navbar.svelte
@@ -1,9 +1,11 @@
 <script>
 	import Logo from "./Logo.svelte";
+  import LanguagePicker from "./LanguagePicker.svelte";
 </script>
 
-<div class="w-full flex justify-center items-center my-4">
+<div class="w-full flex justify-around items-center my-4">
 	<div class="w-12">
     <Logo/>
 	</div>
+  <LanguagePicker/>
 </div>
