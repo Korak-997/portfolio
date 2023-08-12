2b542ab50a99ccfe9f3774f20e9f53ca1c2899af
dani-win-desk <dani.linux.97@gmail.com>
2023-08-12 23:31:39 +0200

    Remove timeline title

--- src/components/sections/TimelineSection.vue
+++ src/components/sections/TimelineSection.vue
@@ -56,18 +56,6 @@
   <section class="text-accent">
     <div class="container max-w-5xl px-4 py-12 mx-auto">
       <div class="grid gap-4 mx-4 sm:grid-cols-12">
-        <div class="col-span-12 sm:col-span-3">
-          <div
-            class="text-center sm:text-left mb-14 before:block before:w-24 before:h-3 before:mb-5 before:rounded-md before:mx-auto sm:before:mx-0 before:dark:bg-primary"
-          >
-            <h3 class="md:text-6xl text-3xl font-semibold">
-              {{ $t("timeline.title") }}
-            </h3>
-            <span class="text-sm font-bold tracki uppercase dark:text-gray-400"
-              >-----</span
-            >
-          </div>
-        </div>
         <div class="relative col-span-12 px-4 space-y-6 sm:col-span-9">
           <div
             class="col-span-12 space-y-12 relative px-4 sm:col-span-8 sm:space-y-8 sm:before:absolute sm:before:top-2 sm:before:bottom-0 sm:before:w-0.5 sm:before:-left-3 before:dark:bg-gray-700"
