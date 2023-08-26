4cdc1d6a32e4783f865703310cad2c85ddc74374
dani-lin-desk <dani.linux.97@gmail.com>
2023-08-26 21:42:25 +0200

    Make intro section simpler

--- src/components/sections/IntroSection.vue
+++ src/components/sections/IntroSection.vue
@@ -4,97 +4,51 @@
   };
 </script>
 <template>
-  <div class="h-screen">
-    <div class="wraper w-screen text-primary flex">
-      <div
-        class="noise pointer-events-none absolute w-full h-full bg-no-repeat bg-cover -z-10"
-      ></div>
-      <div class="overlay pointer-events-none absolute w-full h-full"></div>
-      <div
-        class="terminal uppercase absolute w-full h-full p-4 flex items-center justify-center flex-col"
+  <div
+    class="h-screen w-full flex items-center justify-top flex-col text-center"
+  >
+    <h1 class="font-brice text-6xl mb-6 sh">{{ $t("intro.hi") }}</h1>
+    <h1 class="font-brice text-2xl md:text-4xl mb-6 sh">
+      {{ $t("intro.currentlyWorking") }}
+      <a
+        href="https://www.sincnovation.com/en/"
+        class="text-2xl md:text-4xl cp-link"
+        target="-blank"
+        >SINC NOVATION</a
       >
-        <h1 class="font-brice text-6xl mb-6 sh">{{ $t("intro.hi") }}</h1>
-        <h1 class="font-brice text-4xl mb-6 sh">{{ $t("intro.coding") }}</h1>
-        <div class="my-6">
-          <a
-            href="#socials"
-            class="text-2xl md:text-4xl sh"
-            >[{{ $t("common.reachout") }}]</a
-          >
-        </div>
-        <div class="my-6">
-          <a
-            href="#timeline"
-            class="text-2xl md:text-4xl sh"
-            >[{{ $t("common.readMore") }}]</a
-          >
-        </div>
-      </div>
+    </h1>
+    <h1 class="font-brice text-4xl mb-6 sh">{{ $t("intro.coding") }}</h1>
+    <div class="flex items-center justify-around flex-wrap gap-6">
+      <a
+        href="#socials"
+        class="text-2xl md:text-4xl sh"
+        >[{{ $t("common.reachout") }}]</a
+      >
+      <a
+        href="#timeline"
+        class="text-2xl md:text-4xl sh"
+        >[{{ $t("common.readMore") }}]</a
+      >
     </div>
   </div>
 </template>
-
-<style lang="scss" scooped>
-  .wraper {
-    background-color: #000000;
-    background-image: radial-gradient(#11581e, #041607),
-      url("https://media.giphy.com/media/oEI9uBYSzLpBK/giphy.gif");
+<style scoped>
+  .cp-link {
+    text-shadow: none;
+    background-image: url("../../assets/sincBg.png");
     background-repeat: no-repeat;
-    background-size: cover;
-    text-shadow: 0 0 1ex rgba(51, 255, 51, 1), 0 0 2px rgba(255, 255, 255, 0.8);
-    .noise {
-      background-image: url("https://media.giphy.com/media/oEI9uBYSzLpBK/giphy.gif");
-      opacity: 0.02;
-    }
-    .overlay {
-      background: repeating-linear-gradient(
-        180deg,
-        rgba(0, 0, 0, 0) 0,
-        rgba(0, 0, 0, 0.3) 50%,
-        rgba(0, 0, 0, 0) 100%
-      );
-      background-size: auto 4px;
-      z-index: 1;
-      &::before {
-        content: "";
-        pointer-events: none;
-        position: absolute;
-        display: block;
-        top: 0;
-        left: 0;
-        right: 0;
-        bottom: 0;
-        width: 100%;
-        height: 100%;
-        background-image: linear-gradient(
-          0deg,
-          transparent 0%,
-          rgba(32, 128, 32, 0.2) 2%,
-          rgba(32, 128, 32, 0.8) 3%,
-          rgba(32, 128, 32, 0.2) 3%,
-          transparent 100%
-        );
-        background-repeat: no-repeat;
-        animation: scan 7.5s linear 0s infinite;
-      }
-    }
-
-    .output {
-      color: rgba(128, 255, 128, 0.8);
-      text-shadow: 0 0 1px rgba(51, 255, 51, 0.4),
-        0 0 2px rgba(255, 255, 255, 0.8);
-      &::before {
-        content: "> ";
-      }
-    }
+    -webkit-background-clip: text;
+    -webkit-text-fill-color: transparent;
+    font-weight: bold;
+    background-position: bottom;
+    text-transform: uppercase;
+    font-family: "Steelfish Rg", "helvetica neue", helvetica, arial, sans-serif;
+    font-weight: 800;
+    -webkit-font-smoothing: antialiased;
   }
-
-  @keyframes scan {
-    0% {
-      background-position: 0 -100vh;
-    }
-    100% {
-      background-position: 0 100vh;
-    }
+  .cp-link:hover {
+    transition: all ease-in 1s;
+    cursor: pointer;
+    background-position: top;
   }
 </style>
