f373d44b400440a96b5cce39e01b3b171f77e31c
Korak Kurani <korak.kurani@sincnovation.com>
2023-11-01 11:21:06 +0100

    Add better color schema

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,15 +5,34 @@ module.exports = {
     themes: [
       {
         dark: {
-          primary: "#7ed957",
+          primary: "#7fd959",
 
-          secondary: "#00ffe4",
+          secondary: "#0d013c",
 
-          accent: "#1de4c0",
+          accent: "#4fbf9e",
 
-          neutral: "#ffffff",
+          neutral: "#f5f4fb",
 
-          "base-100": "#0B0B0B",
+          "base-100": "#000000",
+
+          info: "#576fd1",
+
+          success: "#218770",
+
+          warning: "#f0bf75",
+
+          error: "#e92b54",
+        },
+        light: {
+          primary: "#4ca626",
+
+          secondary: "#cfc3fe",
+
+          accent: "#40b08e",
+
+          neutral: "#05040b",
+
+          "base-100": "#ffffff",
 
           info: "#576fd1",
 
