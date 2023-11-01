59f6ff67d3cc02d3fd64433b2c83a4646fb87386
Korak Kurani <korak.kurani@sincnovation.com>
2023-11-01 13:50:40 +0100

    Add project card

--- src/store/index.js
+++ src/store/index.js
@@ -1,9 +1,40 @@
 import { createStore } from "vuex";
 
 export default createStore({
-  state: {},
-  getters: {},
-  mutations: {},
-  actions: {},
-  modules: {},
+  state: {
+    projects: [
+      {
+        id: "001",
+        title: "Dream Style",
+        description: "projects.001.des",
+        tags: [
+          {
+            name: "In Progress",
+            color: "indigo",
+          },
+          {
+            name: "Online",
+            color: "green",
+          },
+          {
+            name: "Responsive",
+            color: "blue",
+          },
+        ],
+        links: [
+          {
+            name: "projects.live",
+            url: "https://dream-style.vercel.app/",
+          },
+          {
+            name: "Github",
+            url: "https://github.com/Korak-997/dreamStyle",
+          },
+        ],
+      },
+    ],
+  },
+  getters: {
+    getProjects: (state) => state.projects,
+  },
 });
