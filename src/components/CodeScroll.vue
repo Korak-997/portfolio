8c8ddf0980a5113c986d8e45aa443ad739720767
Korak Kurani <korak.kurani@sincnovation.com>
2024-02-06 16:08:11 +0100

    Make intro section look better

---
+++ src/components/CodeScroll.vue
@@ -0,0 +1,80 @@
+<script>
+  export default {
+    name: "CodeScroll",
+  };
+</script>
+<template>
+  <div class="loading-blurbs md:text-4xl text-xl w-full">
+    <code class="html">
+      <span class="text-blue-600">&lt;h1</span>
+      <span class="text-cyan-500"> class</span> =
+      <span class="text-orange-600">"vip"</span>&gt;
+      <span class="text-neutral"
+        ><strong>{{ $t("intro.hi") }}</strong></span
+      ><span class="text-blue-600">&lt;/h1&gt;</span>
+    </code>
+    <code class="ruby">
+      <span class="text-neutral">puts</span>
+      <span class="text-orange-600">
+        "<strong>Fullstack Developer</strong>"</span
+      >
+    </code>
+    <code class="js">
+      <span class="text-purple-700">console</span
+      ><span class="text-neutral">.log(</span>
+      <span class="text-orange-600">"<strong>Frontend Developer</strong>"</span>
+      <span class="text-neutral">)</span>
+    </code>
+    <code class="python">
+      <span class="text-green-600">print</span>
+      <span class="text-neutral">(</span>
+      <span class="text-orange-600">"<strong>Youtuber</strong>"</span>
+      <span class="text-neutral">)</span>
+    </code>
+  </div>
+</template>
+<style lang="scss" scoped>
+  .loading-blurbs code {
+    --animation-duration: 2.5s;
+    display: block;
+    position: absolute;
+    width: 100%;
+    bottom: 0;
+    animation-name: move;
+    animation-duration: calc(var(--animation-duration) * 4);
+    animation-timing-function: ease-in-out;
+    animation-iteration-count: infinite;
+    opacity: 0;
+  }
+
+  @keyframes move {
+    from {
+      transform: translateY(0);
+      opacity: 0;
+    }
+    50% {
+      opacity: 1;
+    }
+    70% {
+      opacity: 0;
+    }
+    100% {
+      transform: translateY(-400px);
+    }
+  }
+
+  .loading-blurbs code.html {
+    animation-delay: calc(var(--animation-duration));
+  }
+  .loading-blurbs code.ruby {
+    animation-delay: calc(var(--animation-duration) * 2);
+  }
+
+  .loading-blurbs code.js {
+    animation-delay: calc(var(--animation-duration) * 3);
+  }
+
+  .loading-blurbs code.python {
+    animation-delay: calc(var(--animation-duration) * 4);
+  }
+</style>
--- src/sections/IntroSection.vue
+++ src/sections/IntroSection.vue
@@ -1,72 +1,32 @@
 <script>
   import CustBtn from "@/components/CustBtn.vue";
   import SocialsBar from "@/components/SocialsBar.vue";
+  import CodeScroll from "../components/CodeScroll.vue";
   export default {
     name: "IntroSection",
-    components: { CustBtn, SocialsBar },
-    data() {
-      return {
-        titles: [
-          "korak",
-          "Web Dev",
-          "Full-stack dev",
-          "Youtuber",
-          "Data Processor",
-        ],
-        colors: [
-          "bg-indigo-700 shadow-indigo-500/50",
-          "bg-purple-700 shadow-purple-500/50",
-          "bg-orange-600 shadow-orange-400/50",
-          "bg-red-700 shadow-red-500/50",
-          "bg-cyan-700 shadow-cyan-500/50",
-        ],
-      };
-    },
+    components: { CustBtn, SocialsBar, CodeScroll },
   };
 </script>
 <template>
-  <div class="flex items-center justify-around text-center gap-10 h-full">
-    <div
-      class="uppercase font-bold text-xl md:text-4xl flex items-center justify-around flex-col gap-6 h-full"
-    >
-      <div>
-        <p>{{ $t("intro.hi") }}</p>
-        <section class="animation h-10 overflow-hidden">
-          <div
-            v-for="(title, idx) in titles"
-            :key="idx"
-            :class="colors[idx]"
-            class="text-center p-2 shadow-lg m-4"
-          >
-            <p>{{ title }}</p>
-          </div>
-        </section>
-      </div>
-      <h1 class="text-2xl md:text-4xl">
-        {{ $t("intro.currentlyWorking") }}
-        <a
-          href="https://www.sincnovation.com/en/"
-          class="text-2xl md:text-4xl cp-link"
-          target="-blank"
-          >SINC NOVATION</a
-        >
-      </h1>
-      <div class="flex items-center justify-around flex-wrap w-11/12">
-        <CustBtn
-          :text="'common.reachout'"
-          :link="'#socials'"
-        />
-        <CustBtn
-          :text="'common.readMore'"
-          :link="'/about'"
-        />
-        <CustBtn
-          :text="'common.projects'"
-          :link="'#projects'"
-        />
-      </div>
-      <SocialsBar :onlyIcon="true" />
+  <div
+    class="flex items-center justify-around flex-col text-center gap-4 h-full"
+  >
+    <SocialsBar :onlyIcon="true" />
+    <div class="flex items-center justify-around flex-wrap w-11/12">
+      <CustBtn
+        :text="'common.reachout'"
+        :link="'#socials'"
+      />
+      <CustBtn
+        :text="'common.readMore'"
+        :link="'/about'"
+      />
+      <CustBtn
+        :text="'common.projects'"
+        :link="'#projects'"
+      />
     </div>
+    <CodeScroll />
   </div>
 </template>
 <style scoped lang="scss">
@@ -89,54 +49,5 @@
   p {
     text-shadow: 0 0 7px rgba(255, 255, 255, 0.3),
       0 0 3px rgba(255, 255, 255, 0.3);
-  }
-
-  .animation {
-    div {
-      &:first-child {
-        animation: text-animation 20s infinite;
-      }
-    }
-  }
-
-  @keyframes text-animation {
-    0% {
-      margin-top: 0rem;
-    }
-    30% {
-      margin-top: -5.5rem;
-    }
-    40% {
-      margin-top: -10.5rem;
-    }
-    50% {
-      margin-top: -15.5rem;
-    }
-    60% {
-      margin-top: -20.5rem;
-    }
-    70% {
-      margin-top: -15.5rem;
-    }
-    80% {
-      margin-top: -10.5rem;
-    }
-    90% {
-      margin-top: -5.5rem;
-    }
-    100% {
-      margin-top: 0;
-    }
-  }
-
-  @keyframes lights {
-    0% {
-      text-shadow: 0 0 5px #fff, 0 0 1px #fff, 0 0 2px #fff, 0 0 3px #206b0b,
-        0 0 4px #206b0b, 0 0 5px #206b0b, 0 0 10px #206b0b, 0 0 15px #206b0b;
-    }
-    100% {
-      text-shadow: 0 0 5px #fff, 0 0 1px #fff, 0 0 2px #fff, 0 0 3px #39b616,
-        0 0 4px #39b616, 0 0 5px #39b616, 0 0 10px #39b616, 0 0 15px #39b616;
-    }
   }
 </style>
