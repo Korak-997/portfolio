477f54faf3667483d97efe21f917199cd7e81a0b
Korak Kurani <korak.kurani@sincnovation.com>
2024-02-07 10:23:12 +0100

    Add better colors

--- src/components/CustBtn.vue
+++ src/components/CustBtn.vue
@@ -16,7 +16,7 @@
   <a
     v-if="out"
     :href="link"
-    class="relative py-2 px-8 text-white text-base font-bold nded-full overflow-hidden bg-primary rounded-full transition-all duration-400 ease-in-out shadow-md hover:scale-105 hover:text-white hover:shadow-lg active:scale-90 before:absolute before:top-0 before:-left-full before:w-full before:h-full before:bg-gradient-to-r before:from-accent before:to-primary before:transition-all before:duration-500 before:ease-in-out before:z-[-1] before:rounded-full hover:before:left-0"
+    class="relative py-2 px-8 text-white text-base font-bold nded-full overflow-hidden bg-accent rounded-full transition-all duration-400 ease-in-out shadow-md hover:scale-105 hover:text-white hover:shadow-lg active:scale-90 before:absolute before:top-0 before:-left-full before:w-full before:h-full before:bg-gradient-to-r before:from-secondary before:to-accent before:transition-all before:duration-500 before:ease-in-out before:z-[-1] before:rounded-full hover:before:left-0"
     :aria-label="link"
     target="_blank"
     >{{ $t(`${text}`)
@@ -28,7 +28,7 @@
   <a
     v-else
     :href="link"
-    class="relative py-2 px-8 text-white text-base font-bold nded-full overflow-hidden bg-primary rounded-full transition-all duration-400 ease-in-out shadow-md hover:scale-105 hover:text-white hover:shadow-lg active:scale-90 before:absolute before:top-0 before:-left-full before:w-full before:h-full before:bg-gradient-to-r before:from-accent before:to-primary before:transition-all before:duration-500 before:ease-in-out before:z-[-1] before:rounded-full hover:before:left-0"
+    class="relative py-2 px-8 text-white text-base font-bold nded-full overflow-hidden bg-accent rounded-full transition-all duration-400 ease-in-out shadow-md hover:scale-105 hover:text-white hover:shadow-lg active:scale-90 before:absolute before:top-0 before:-left-full before:w-full before:h-full before:bg-gradient-to-r before:from-secondary before:to-accent before:transition-all before:duration-500 before:ease-in-out before:z-[-1] before:rounded-full hover:before:left-0"
     >{{ $t(`${text}`) }}</a
   >
 </template>
