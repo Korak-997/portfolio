3741dea27ae04388a6b79388e2d30a7f335035d9
dani-lin-desk <dani.linux.97@gmail.com>
2024-03-04 19:35:31 +0100

    Add better hero text

--- src/sections/IntroSection.vue
+++ src/sections/IntroSection.vue
@@ -1,6 +1,7 @@
 <script>
   import CustBtn from "@/components/CustBtn.vue";
   import particlesConfig from "../assets/particles-js.json";
+  import TextScramble from "../scrimble";
   export default {
     name: "IntroSection",
     components: { CustBtn },
@@ -11,6 +12,22 @@
     },
     mounted() {
       particlesJS.load("particles-js", JSON.stringify(this.configs));
+      const phrases = [
+        "Korak",
+        "Fullstack Developer",
+        "Youtuber",
+        "Kurdish Guy",
+      ];
+      const el = document.querySelector(".text");
+      const fx = new TextScramble(el);
+      let counter = 0;
+      const next = () => {
+        fx.setText(phrases[counter]).then(() => {
+          setTimeout(next, 800);
+        });
+        counter = (counter + 1) % phrases.length;
+      };
+      next();
     },
   };
 </script>
@@ -28,32 +45,9 @@
       id="particles-js"
       class="absolute w-full h-full"
     ></div>
+    <h1 class="text-6xl font-extrabold">{{ $t("intro.hi") }}</h1>
+    <div class="text text-4xl md:text-4xl lg:text-4xl"></div>
 
-    <div class="p-2">
-      <h1 class="text-xl md:text-4xl">
-        <span class="text-blue-600">&lt;h1</span>
-        <span class="text-cyan-500"> class</span> =
-        <span class="text-orange-600">"vip"</span>&gt;
-        <span class="text-neutral">{{ $t("intro.hi") }}</span
-        ><span class="text-blue-600">&lt;/h1&gt;</span>
-      </h1>
-      <h1 class="text-xl md:text-4xl">
-        <span class="text-neutral">puts</span>
-        <span class="text-orange-600"> "Fullstack Developer"</span>
-      </h1>
-      <h1 class="text-xl md:text-4xl">
-        <span class="text-purple-700">console</span
-        ><span class="text-neutral">.log(</span>
-        <span class="text-orange-600">"Frontend Developer"</span>
-        <span class="text-neutral">)</span>
-      </h1>
-      <h1 class="text-xl md:text-4xl">
-        <span class="text-green-600">print</span>
-        <span class="text-neutral">(</span>
-        <span class="text-orange-600">"Youtuber"</span>
-        <span class="text-neutral">)</span>
-      </h1>
-    </div>
     <div class="flex items-center justify-around flex-wrap w-11/12">
       <CustBtn
         :text="'common.reachout'"
@@ -66,25 +60,4 @@
     </div>
   </div>
 </template>
-<style scoped lang="scss">
-  .cp-link {
-    background-image: url("@/assets/sincBg.webp");
-    text-shadow: none;
-    background-repeat: no-repeat;
-    background-clip: text;
-    color: transparent;
-    font-weight: bold;
-    background-position: center;
-    text-transform: uppercase;
-
-    &:hover {
-      transition: all ease-in 1s;
-      cursor: pointer;
-      background-position: top;
-    }
-  }
-  p {
-    text-shadow: 0 0 7px rgba(255, 255, 255, 0.3),
-      0 0 3px rgba(255, 255, 255, 0.3);
-  }
-</style>
+<style scoped lang="scss"></style>
