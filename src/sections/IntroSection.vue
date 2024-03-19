02bdd464b5d5c3b25b88904b9d3de4473fdcfd68
dani-lin-desk <dani.linux.97@gmail.com>
2024-03-19 18:57:57 +0100

    Fix some small bugs

--- src/sections/IntroSection.vue
+++ src/sections/IntroSection.vue
@@ -17,7 +17,7 @@
       let counter = 0;
       const next = () => {
         fx.setText(phrases[counter]).then(() => {
-          setTimeout(next, 800);
+          setTimeout(next, 2000);
         });
         counter = (counter + 1) % phrases.length;
       };
@@ -27,22 +27,15 @@
 </script>
 <template>
   <div
-    class="flex items-center justify-center flex-col text-center gap-20 h-full"
+    class="flex items-center justify-center flex-col text-center gap-20 h-full w-full"
   >
     <h1 class="text-6xl font-extrabold">{{ $t("intro.hi") }}</h1>
-    <div class="text text-4xl md:text-4xl lg:text-4xl"></div>
+    <div class="text text-4xl md:text-6xl lg:text-6xl w-full"></div>
 
-    <div class="flex items-center justify-around flex-wrap w-11/12">
-      <CustBtn
-        :text="'common.reachout'"
-        :link="'#socials'"
-        colorClasses="hover:text-primary before:bg-primary after:bg-indigo-500 hover:border-indigo-500"
-      />
-      <CustBtn
-        :text="'common.projects'"
-        :link="'#projects'"
-        colorClasses="hover:text-secondary before:bg-secondary after:bg-cyan-600 hover:border-cyan-600"
-      />
-    </div>
+    <CustBtn
+      :text="'common.projects'"
+      :link="'#projects'"
+      colorClasses="hover:text-secondary before:bg-secondary after:bg-cyan-600 hover:border-cyan-600"
+    />
   </div>
 </template>
