6b701004b8117b58896c9260e619ca6988f5b93b
dani-lin-desk <dani.linux.97@gmail.com>
2024-03-27 18:54:34 +0100

    Add footer for small screens

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import { Icon } from "@iconify/react";
 function App() {
   return (
     <div className="w-full flex items-center justify-around flex-col gap-6">
-      <nav className="p-4 flex items-center justify-around w-full h-20 shadow-xl">
+      <nav className="p-4 lg:flex md:flex items-center justify-between w-full h-20 shadow-xl hidden">
         <img
           src={Logo}
           alt="logo"
@@ -30,6 +30,27 @@ function App() {
           </a>
         </div>
       </nav>
+
+      <footer className="flex lg:hidden md:hidden items-center justify-between w-full h-16 absolute bottom-0 z-10">
+        <a
+          href="#projects"
+          className="text-primary hover:bg-primary hover:text-background text-4xl flex-1 flex items-center justify-center h-full"
+        >
+          <Icon icon="grommet-icons:projects" />
+        </a>
+        <a
+          href="#about"
+          className="text-primary hover:bg-primary hover:text-background text-4xl flex-1 flex items-center justify-center h-full"
+        >
+          <Icon icon="mdi:about-variant" />
+        </a>
+        <a
+          href="#contact"
+          className="text-primary hover:bg-primary hover:text-background text-4xl flex-1 flex items-center justify-center h-full"
+        >
+          <Icon icon="mdi:message" />
+        </a>
+      </footer>
     </div>
   );
 }
