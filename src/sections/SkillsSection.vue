04c3b33d21fb040a345ba6dae72ad049b2f329b4
dani-lin-lp <dani.linux.97@gmail.com>
2024-04-04 12:53:00 +0200

    Remove duplicate class names

--- src/sections/SkillsSection.vue
+++ src/sections/SkillsSection.vue
@@ -26,7 +26,7 @@
         :class="`rounded-2xl shadow-sm ${skill.color1}`"
       >
         <div
-          :class="`${skill.color2} group overflow-hidden relative after:duration-500 before:duration-500 duration-500 hover:after:duration-500 hover:after:translate-x-24 hover:before:translate-y-12 hover:before:-translate-x-32 hover:duration-500 after:absolute after:w-24 after:h-24  after:rounded-full after:blur-xl after:bottom-32 after:right-16 after:w-12 after:h-12 before:absolute before:w-20 before:h-20  before:rounded-full before:blur-xl before:top-20 before:right-16 before:w-12 before:h-12 flex justify-center items-center h-56 w-80 origin-bottom-right bg-neutral-900 rounded-2xl outline outline-slate-400 -outline-offset-8`"
+          :class="`${skill.color2} group overflow-hidden relative after:duration-500 before:duration-500 duration-500 hover:after:duration-500 hover:after:translate-x-24 hover:before:translate-y-12 hover:before:-translate-x-32 hover:duration-500 after:absolute after:w-24 after:h-24  after:rounded-full after:blur-xl after:bottom-32 after:right-16 before:absolute before:w-20 before:h-20  before:rounded-full before:blur-xl before:top-20 before:right-16 flex justify-center items-center h-56 w-80 origin-bottom-right bg-neutral-900 rounded-2xl outline outline-slate-400 -outline-offset-8`"
         >
           <div class="z-10 flex flex-col items-center gap-2">
             <span class="text-6xl font-bold"><Icon :icon="skill.icon" /></span>
